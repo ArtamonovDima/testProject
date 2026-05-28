5971058dc953bd4dfd8111a1b8de6d90b48d7d42
Dmytro Artamonov <dima.artamonov1989@gmail.com>
2026-05-28 13:21:20 +0200

    add direct API test

--- src/test/java/api/FilmApiTest.java
+++ src/test/java/api/FilmApiTest.java
@@ -2,7 +2,11 @@ package api;
 
 import api.service.FilmService;
 import api.util.FilmValidator;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
 import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FilmApiTest {
@@ -28,5 +32,29 @@ public class FilmApiTest {
 
         assertThat(film.title()).isEqualTo(titleForTest);
         FilmValidator.validateTitle(film, titleForTest);
+    }
+
+    @Test
+    public void directTest() {
+
+        var yearForTest = "1977";
+        var response =
+                given()
+                        .spec(new RequestSpecBuilder().setBaseUri("https://swapi.dev/api")
+                                .setRelaxedHTTPSValidation()
+                                .addHeader("Content-Type", "application/json")
+                                .build())
+                        .when().get("/films")
+                        .then()
+                        .spec(new ResponseSpecBuilder().expectStatusCode(200)
+                                .build())
+                        .extract().response();
+
+        //for 'var' we need to add String.class, in other way it will be the Object type
+        var releaseDates = response.jsonPath().getList("results.release_date", String.class);
+
+        assertThat(releaseDates)
+                .isNotEmpty()
+                .anyMatch(date -> date.contains(yearForTest));
     }
 }
